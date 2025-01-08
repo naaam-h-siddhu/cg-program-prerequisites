b7857809478ce7147cd9c19cf81c509ff99e8f5d
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-08 12:39:09 +0530

    Added Average Caclulator Program

---
+++ Average.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Average{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        System.out.println("Enter the Third number");
+        int c = sc.nextInt();
+        System.out.println("The Average is :" +(a+b+c)/3);
+    }
+}
+
