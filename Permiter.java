45284fc752226a98b312309d86eaab2a51b152ac
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-08 12:27:06 +0530

    Added Perimeter of Rectangle Program

---
+++ Permiter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Permiter{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the lenght of rectangle : ");
+        int l = sc.nextInt();
+        System.out.println("enter the breadth of rectange : ");
+        int b = sc.nextInt();
+        System.out.println("Area of Rectangle = "+ l*b);
+    }
+}
